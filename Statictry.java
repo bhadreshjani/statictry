a8f44eac99de450602a6064d15244e5be9236d3b
bhadreshjani <nilkanthim@gmail.com>
2016-10-06 11:15:22 -0700

    Add files via upload

---
+++ Statictry.java
@@ -0,0 +1,13 @@
+
+public class Statictry {
+	static
+	{
+		print(10);
+	} 
+	static void print (int x)
+	{
+		System.out.println (x);
+		System.exit(0);
+		}
+}
+// i did try it is not compiling and even i do not know what hapned it runs previous class only
